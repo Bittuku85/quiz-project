2fe2bb22491e1895eb287a26183694cc4e8f6ed6
Bittuku85 <bittukumar1666382@gmail.com>
2025-01-20 19:23:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Show Sign Up form
+function showSignUpForm() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+}
+
+// Show Login form
+function showLoginForm() {
+    document.getElementById("signup-form").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+}
